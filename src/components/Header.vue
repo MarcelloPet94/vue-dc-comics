d0d78d07e5349b7a2919270105c93810333e3502
marcello <marcellopettenuzzo2@gmail.com>
2022-01-27 15:41:27 +0100

    struttura - header/main/footer

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    
+      <div class="logo_img"></div>
+      <nav></nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+
+  .header
+  {
+    height: 100px;
+    background-color: lightgrey;
+
+  }
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Main</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
